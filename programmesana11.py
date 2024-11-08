45a010427375d201c2834dbb5f13d295a8f3843b
ievaekarklina <ievakarklina746@gmail.com>
2024-11-08 18:40:50 +0200

    Add files via upload

---
+++ programmesana11.py
@@ -0,0 +1,19 @@
+x=int(input("x:"))
+y=int(input("y:"))
+result 2=5**3-x**2+7x*x-6
+print(result)
+
+x=int(input("x:"))
+y=int(input("y"))
+result= math.sqrt(x*y)
+print(result)
+
+x=int(input("x:"))
+y=int(input("y"))
+result=(2**(x*y))/(5*y)
+print(result)
+
+x=int(input("x:"))
+y=int(input("y:"))
+result= 2+x/xy
+print(result)
